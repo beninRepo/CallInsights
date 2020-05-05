980294c95ceaf729f16eb002aa9b4cef8fa1a509
benin0710 <b.paul@evision-software.com>
2020-05-05 19:19:35 +0200

    Removed searchbox

--- src/Layout/AppHeader/index.js
+++ src/Layout/AppHeader/index.js
@@ -34,9 +34,7 @@ class Header extends React.Component {
                         "app-header__content",
                         {'header-mobile-open': enableMobileMenuSmall},
                     )}>
-                        <div className="app-header-left">
-                            <SearchBox/>
-                        </div>
+                        
                         <div className="app-header-right">
                             <UserBox/>
                         </div>
