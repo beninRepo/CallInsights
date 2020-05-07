bccfe22e235d5e1a02e61384310ac3cbfce79209
benin0710 <b.paul@evision-software.com>
2020-05-08 00:48:15 +0200

    Added chart export functionality to department and extension charts

--- src/DemoPages/Charts/ChartJs/Examples/DepartmentCostChart.js
+++ src/DemoPages/Charts/ChartJs/Examples/DepartmentCostChart.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import html2canvas from "html2canvas";
+const pdfConverter = require("jspdf");
 import {
   ResponsiveContainer, ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
@@ -12,9 +14,28 @@ const data2 = [
 ];
 
 export default class DepartmentCostChart extends PureComponent {
- 
+    div2PDF = e => {
+        const but = e.target;
+        let input = window.document.getElementsByClassName("divDepartmentCost")[0];
+
+        html2canvas(input).then(canvas => {
+            const img = canvas.toDataURL("image/png");
+            const pdf = new pdfConverter("l", "pt");
+            pdf.addImage(
+                img,
+                "png",
+                input.offsetLeft,
+                input.offsetTop,
+                input.clientWidth,
+                input.clientHeight
+            );
+            pdf.save("DepartmentCostChart.pdf");
+        });
+    };
   render() {
-    return (
+      return (
+          <div>
+              <div className="divDepartmentCost">
         <div className="widget-chart p-0">
             <ResponsiveContainer height={179}>
                 <ComposedChart data={data2}>
@@ -28,7 +49,11 @@ export default class DepartmentCostChart extends PureComponent {
                 </ComposedChart>
             </ResponsiveContainer>
 
-        </div>
+                  </div>
+                  <hr />
+              </div>
+              <button className='mb-2 mr-2 btn-hover-shine btn btn-warning' onClick={(e) => this.div2PDF(e)}>Save PDF</button>
+          </div>
     );
   }
 }
--- src/DemoPages/Charts/ChartJs/Examples/ExtensionChart.js
+++ src/DemoPages/Charts/ChartJs/Examples/ExtensionChart.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import html2canvas from "html2canvas";
+const pdfConverter = require("jspdf");
 import {
     ResponsiveContainer, ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
@@ -14,13 +16,34 @@ const data2 = [
 
 ];
 
-export default class ExtensionChart extends PureComponent {
 
+export default class ExtensionChart extends React.Component {
+    div2PDF = e => {
+        const but = e.target;
+        let input = window.document.getElementsByClassName("extensionChart")[0];
+
+        html2canvas(input).then(canvas => {
+            const img = canvas.toDataURL("image/png");
+            const pdf = new pdfConverter("l", "pt");
+            pdf.addImage(
+                img,
+                "png",
+                input.offsetLeft,
+                input.offsetTop,
+                input.clientWidth,
+                input.clientHeight
+            );
+            pdf.save("Extensionchart.pdf");
+            });
+    };
     render() {
         return (
-            <div className="widget-chart p-0">
+            <div>
+            <div className="extensionChart">
+            <div className="widget-chart p-0">               
                 <ResponsiveContainer height={179}>
-                    <ComposedChart data={data2}>
+                    <ComposedChart data={data2} >
+                        
                         <CartesianGrid stroke="#ffffff" />
                         <XAxis dataKey="name" />
                         <YAxis  />
@@ -28,10 +51,16 @@ export default class ExtensionChart extends PureComponent {
                         <Area type="monotone" dataKey="count" fill="#f7ffd0" stroke="#85a200" />
                         <Bar dataKey="cost" barSize={16} fill="var(--primary)" />
 
-                    </ComposedChart>
+                    </ComposedChart>                 
+
                 </ResponsiveContainer>
 
+                    </div>
+                    <hr/>
+            </div>           
+                <button className='mb-2 mr-2 btn-hover-shine btn btn-warning' onClick={(e) => this.div2PDF(e)}>Save PDF</button>
             </div>
+          
         );
     }
 }
